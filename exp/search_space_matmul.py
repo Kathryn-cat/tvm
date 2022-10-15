7773cc09c1cf511ae8aa5b1277df9a9e013a74e5
Jinqi Chen <jinqic@andrew.cmu.edu>
2022-10-15 17:13:31 -0400

    fixed issue with analyzer

--- exp/search_space_matmul.py
+++ exp/search_space_matmul.py
@@ -62,30 +62,27 @@ def schedule_matmul(sch: tir.Schedule) -> None:
     # split l_g1
     v1_g1 = sch.sample_categorical(candidates=cand, probs=prob)
     l_g11, l_g1r = sch.split(loop=l_g1, factors=[None, v1_g1])
-    res = sch.sample_perfect_tile(loop=l_g1r, n=4, max_innermost_factor=4)
-    l_g12, l_g13, l_g14, l_g15 = sch.split(loop=l_g1r, factors=[*res])
+    res = sch.sample_perfect_tile(loop=l_g1r, n=3, max_innermost_factor=4)
+    l_g12, l_g13, l_g14 = sch.split(loop=l_g1r, factors=[*res])
     # split l_g2
     v1_g2 = sch.sample_categorical(candidates=cand, probs=prob)
     l_g21, l_g2r = sch.split(loop=l_g2, factors=[None, v1_g2])
-    res = sch.sample_perfect_tile(loop=l_g2r, n=4, max_innermost_factor=4)
-    l_g22, l_g23, l_g24, l_g25 = sch.split(loop=l_g2r, factors=[*res])
+    res = sch.sample_perfect_tile(loop=l_g2r, n=3, max_innermost_factor=4)
+    l_g22, l_g23, l_g24 = sch.split(loop=l_g2r, factors=[*res])
     # split l_g3
     v1_g3 = sch.sample_categorical(candidates=cand, probs=prob)
     l_g31, l_g3r = sch.split(loop=l_g3, factors=[None, v1_g3])
     res = sch.sample_perfect_tile(loop=l_g3r, n=2, max_innermost_factor=4)
     l_g32, l_g33 = sch.split(loop=l_g3r, factors=[*res])
-    sch.reorder(
-        l_g11, l_g21, l_g12, l_g22, l_g13, l_g23, l_g31, l_g32, l_g14, l_g24, l_g33, l_g15, l_g25
-    )
-    l_g1 = sch.fuse(l_g11, l_g21)
-    l_g2 = sch.fuse(l_g12, l_g22)
-    l_g3 = sch.fuse(l_g13, l_g23)
-    sch.bind(loop=l_g1, thread_axis="blockIdx.y")
-    sch.bind(loop=l_g2, thread_axis="blockIdx.x")
+    # only l_g11, l_g21, l_g31 are dynamic loops
+    sch.reorder(l_g11, l_g21, l_g12, l_g22, l_g31, l_g32, l_g13, l_g23, l_g33, l_g14, l_g24)
+    l_g3 = sch.fuse(l_g12, l_g22)
+    sch.bind(loop=l_g11, thread_axis="blockIdx.y")
+    sch.bind(loop=l_g21, thread_axis="blockIdx.x")
     sch.bind(loop=l_g3, thread_axis="threadIdx.y")
     # cache write
     C_shared = sch.cache_write(b_mm, 0, "shared")
-    sch.reverse_compute_at(C_shared, l_g2)
+    sch.reverse_compute_at(C_shared, l_g21)
     C_local = sch.cache_write(b_mm, 0, "wmma.accumulator")
     sch.reverse_compute_at(C_local, l_g3)
     # cooperative fetch for shared memory
@@ -101,6 +98,8 @@ def schedule_matmul(sch: tir.Schedule) -> None:
     l_l11, l_l12 = sch.split(loop=l_l1, factors=[None, 16])
     l_l21, l_l22 = sch.split(loop=l_l2, factors=[None, 16])
     sch.reorder(l_l21, l_l12)
+    # tensorize
+    sch.tensorize(block_or_loop=l_l12, tensor_intrin="wmma_store_16x16x16_f16_shared")
     # cache read
     A_shared = sch.cache_read(block=b_mm, read_buffer_index=0, storage_scope="shared")
     B_shared = sch.cache_read(block=b_mm, read_buffer_index=1, storage_scope="shared")
@@ -111,46 +110,50 @@ def schedule_matmul(sch: tir.Schedule) -> None:
     l_s1f = sch.fuse(l_s11, l_s12)
     _, _, _, _, l_s21, l_s22 = sch.get_loops(B_shared)
     l_s2f = sch.fuse(l_s21, l_s22)
-    # cooperative fetch for shared memory
+    # cooperative fetch for shared memory for A
     _, _, _, _, l_s = sch.get_loops(block=A_shared)
     v_s = sch.sample_categorical(candidates=cand, probs=prob)
     l_s1, l_s2, l_s3, l_s4 = sch.split(loop=l_s, factors=[None, sch.get(l_g3).extent, 32, v_s])
     sch.vectorize(loop=l_s4)
     sch.bind(loop=l_s3, thread_axis="threadIdx.x")
     sch.bind(loop=l_s2, thread_axis="threadIdx.y")
+    # cooperative fetch for shared memory for B
     _, _, _, _, l_s = sch.get_loops(block=B_shared)
     v_s = sch.sample_categorical(candidates=cand, probs=prob)
     l_s1, l_s2, l_s3, l_s4 = sch.split(loop=l_s, factors=[None, sch.get(l_g3).extent, 32, v_s])
     sch.vectorize(loop=l_s4)
     sch.bind(loop=l_s3, thread_axis="threadIdx.x")
     sch.bind(loop=l_s2, thread_axis="threadIdx.y")
+    # cache read for wmma
     A_local = sch.cache_read(block=b_mm, read_buffer_index=0, storage_scope="wmma.matrix_a")
     B_local = sch.cache_read(block=b_mm, read_buffer_index=1, storage_scope="wmma.matrix_b")
     sch.compute_at(block=A_local, loop=l_g32)
     sch.compute_at(block=B_local, loop=l_g32)
-    # split loops for local memory caching when reading to align with tensor core
+    # split loops for local memory caching when reading to align with tensor core for A
     _, _, _, _, _, l_l1, l_l2 = sch.get_loops(block=A_local)
     l_l11, l_l12 = sch.split(loop=l_l1, factors=[None, 16])
     l_l21, l_l22 = sch.split(loop=l_l2, factors=[None, 16])
     sch.reorder(l_l21, l_l12)
+    # tensorize
+    sch.tensorize(block_or_loop=l_l12, tensor_intrin="wmma_load_16x16x16_f16_a")
+    # split loops for local memory caching when reading to align with tensor core for B
     _, _, _, _, _, l_l1, l_l2 = sch.get_loops(block=B_local)
     l_l11, l_l12 = sch.split(loop=l_l1, factors=[None, 16])
     l_l21, l_l22 = sch.split(loop=l_l2, factors=[None, 16])
     sch.reorder(l_l21, l_l12)
+    # tensorize
+    sch.tensorize(block_or_loop=l_l12, tensor_intrin="wmma_load_16x16x16_f16_b")
     # unrolling max step
     # decompose reduction
     b_Co = sch.get_block("C_o")
     sch.decompose_reduction(block=b_Co, loop=l_g31)
-    # tensorization
+    # tensorize
     b_ti = sch.get_block("C_init")
     l_ti, _ = sch.get_loops(block=b_ti)
     sch.tensorize(block_or_loop=l_ti, tensor_intrin="wmma_fill_16x16x16_f16")
     b_tc = sch.get_block("C")
     l_tc, _, _ = sch.get_loops(block=b_tc)
     sch.tensorize(block_or_loop=l_tc, tensor_intrin="wmma_sync_16x16x16_f16f16f16_trans")
-    b_ts = sch.get_block("C_shared_wmma.accumulator")
-    _, _, _, _, _, l_ts, _ = sch.get_loops(block=b_ts)
-    # sch.tensorize(block_or_loop=l_ts, tensor_intrin="wmma_store_16x16x16_f16_shared")
 
 
 def apply_trace(sch):
@@ -441,11 +444,11 @@ if __name__ == "__main__":
         apply_trace(sch)
         sch.mod.show()
 
+    """
     print("tensor intrinsic:")
-    res = get_wmma_store_intrin(16, 16, 16, "float16", "shared")
+    res = get_wmma_store_intrin(16, 16, 16, "float16", "load")
     res[0].show()
 
-    """
     # evaluate the running time
     dev = tvm.cuda(0)
     A_np = np.random.uniform(size=(1024, )).astype("float32")
