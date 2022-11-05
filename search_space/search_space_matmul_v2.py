742a59952d1f5708d16a1520223a6118bfcd1256
Kathryn-cat <jinqic@andrew.cmu.edu>
2022-11-05 10:47:53 -0700

    branch desc

--- search_space/search_space_matmul_v2.py
+++ search_space/search_space_matmul_v2.py
@@ -4,6 +4,8 @@ and bridging the gap between static matmul and fully dynamic
 matmul. This file focuses on the case where only the reduction
 loop contains dynamic variable. The method we use is the microkernel
 idea. The goal here is to align performance with the static case. 
+
+Test with this branch with no hacks. (previous hacks in dyn-shape)
 """
 
 
