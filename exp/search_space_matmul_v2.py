c865ee3b6338b1916e4f4d4bea16a23d3bc8932b
Kathryn-cat <jinqic@andrew.cmu.edu>
2022-11-05 10:10:08 -0700

    add stuff

--- exp/search_space_matmul_v2.py
+++ exp/search_space_matmul_v2.py
@@ -23,3 +23,17 @@ Suppose we have a high-performance 128 * 128 * 32 microkernel. This will be
 acquired later from MetaSchedule tuning, but now we just leave it as a
 subregion computation.
 """
+
+
+@T.prim_func
+def matmul(a: T.handle, b: T.handle, c: T.handle, m: T.int32, n: T.int32, p: T.int32) -> None:
+    T.func_attr({"global_symbol": "matmul", "tir.noalias": True})
+    A = T.match_buffer(a, (1024, 512 * n), "float16")
+    B = T.match_buffer(b, (512 * n, 1024), "float16")
+    C = T.match_buffer(c, (1024, 1024), "float16")
+    for i, j, k in T.grid(1024, 512 * n, 1024):
+        with T.block("C"):
+            vi, vj, vk = T.axis.remap("SSR", [i, j, k])
+            with T.init():
+                C[vi, vj] = T.float16(0.0)
+            C[vi, vj] = C[vi, vj] + A[vi, vk] * B[vk, vj]
