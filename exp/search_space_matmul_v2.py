c1c55dcebfad75692313b38e2c603cd1e15bd367
Kathryn-cat <jinqic@andrew.cmu.edu>
2022-11-05 09:02:43 -0700

    begin v2

---
+++ exp/search_space_matmul_v2.py
@@ -0,0 +1,7 @@
+"""
+In this file, I start with handcrafting the intermediate states 
+and bridging the gap between static matmul and fully dynamic 
+matmul. This file focuses on the case where only the reduction
+loop contains dynamic variable. The method we use is the microkernel
+idea. The goal here is to align performance with the static case. 
+"""
