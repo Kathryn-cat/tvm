a18d9c127c8b00dab24d7d3ac34419a8aadd4b91
Kathryn-cat <jinqic@andrew.cmu.edu>
2022-12-20 13:34:45 -0800

    add draft

---
+++ 0-depth/microkernel.py
@@ -0,0 +1,46 @@
+"""
+In this file, we aim to construct a microkernel for one threadblock which has good-enough performance
+per CUTLASS case.
+"""
+
+
+import numpy as np
+import torch
+
+import tvm
+from tvm import meta_schedule as ms
+from tvm import tir
+from tvm.meta_schedule.postproc import Postproc
+from tvm.script import tir as T
+from tvm.tir.tensor_intrin import cuda as _
+from tvm.tir.tensor_intrin.cuda import get_wmma_store_intrin
+
+# we don't have direct performance measurement for one threadblock, so we tune a huge one first.
+
+
+@T.prim_func
+def hgemm_4096(a: T.handle, b: T.handle, c: T.handle) -> None:
+    T.func_attr({"global_symbol": "microkernel", "tir.noalias": True})
+    A = T.match_buffer(a, (4096, 4096), "float16")
+    B = T.match_buffer(b, (4096, 4096), "float16")
+    C = T.match_buffer(c, (4096, 4096), "float16")
+    for i, j, k in T.grid(4096, 4096, 4096):
+        with T.block("C"):
+            vi, vj, vk = T.axis.remap("SSR", [i, j, k])
+            with T.init():
+                C[vi, vj] = T.float16(0.0)
+            C[vi, vj] = C[vi, vj] + A[vi, vk] * B[vk, vj]
+
+
+@T.prim_func
+def microkernel_128_128_32(a: T.handle, b: T.handle, c: T.handle) -> None:
+    T.func_attr({"global_symbol": "microkernel", "tir.noalias": True})
+    A = T.match_buffer(a, (128, 32), "float16")
+    B = T.match_buffer(b, (32, 128), "float16")
+    C = T.match_buffer(c, (128, 128), "float16")
+    for i, j, k in T.grid(128, 128, 32):
+        with T.block("C"):
+            vi, vj, vk = T.axis.remap("SSR", [i, j, k])
+            with T.init():
+                C[vi, vj] = T.float16(0.0)
+            C[vi, vj] = C[vi, vj] + A[vi, vk] * B[vk, vj]
